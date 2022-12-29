6747d598e1702beb9223b8fede48132513f3cc32
dogebi2 <peter.xu@xenera.jp>
2022-12-29 15:44:29 +0800

    first

--- ViewModifer_swiftui/ContentView.swift
+++ ViewModifer_swiftui/ContentView.swift
@@ -9,10 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing:50){
+            Text("Hello, world!")
+                .myRoundedTextStyle()
+            
+            Text("Hello, world!")
+                .myRoundedTextStyle()
+            
+            Image(systemName: "book")
+                .myRoundedTextStyle()
+            
+            Rectangle().frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/).foregroundColor(/*@START_MENU_TOKEN@*/.blue/*@END_MENU_TOKEN@*/)
+                .myRoundedTextStyle()
+        }
+
     }
 }
+
+//뷰를 꾸며주는 모디파이어이다.
+struct MyRoundedText: ViewModifier{
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .padding()
+            .background(Color.yellow)
+            .cornerRadius(20)
+            .padding()
+            .overlay(RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*/25.0/*@END_MENU_TOKEN@*/).stroke(lineWidth: 10).foregroundColor(.red))
+    }
+}
+
+extension View{
+    func myRoundedTextStyle() -> some View{
+        modifier(MyRoundedText())
+    }
+}
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
