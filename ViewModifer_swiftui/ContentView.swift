fe1f598b6ea9970163089f0ed977128872dc1cd6
dogebi2 <peter.xu@xenera.jp>
2022-12-29 15:32:47 +0800

    Initial Commit

---
+++ ViewModifer_swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ViewModifer_swiftui
+//
+//  Created by rainxlh on 2022/12/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ViewModifer_swiftui/ViewModifer_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewModifer_swiftuiApp.swift
+//  ViewModifer_swiftui
+//
+//  Created by rainxlh on 2022/12/29.
+//
+
+import SwiftUI
+
+@main
+struct ViewModifer_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
